1bc3efd0d16cc178f135372a0160fba1ef17cd9e
irvingpercam <a01703171@itesm.mx>
2020-07-24 00:27:43 -0500

    Update: fixed css styles

--- sections/_rozamiento.php
+++ sections/_rozamiento.php
@@ -1,6 +1,6 @@
 <!-- Start Welcome -->
 <div class="container">
-        <h4 class="white-text center" style="font-family:hypertext;font-size:8vw;">Plano inclinado con rozamiento</h4>
+        <h4 class="welcome white-text center">Plano inclinado con rozamiento</h4>
     </div>
         <br>
         <br>
--- sections/_srozamiento.php
+++ sections/_srozamiento.php
@@ -1,6 +1,6 @@
 <!-- Start Welcome -->
 <div class="container">
-        <h4 class="white-text center" style="font-family:hypertext;font-size:8vw;">Plano inclinado sin rozamiento</h4>
+        <h4 class="welcome white-text center">Plano inclinado sin rozamiento</h4>
     </div>
         <br>
         <br>
